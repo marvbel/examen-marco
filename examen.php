80ebc05abe7a9ccf0380b9016e972a3b66b45e6f
marco antonio <lobitosolo77@gmail.com>
2016-03-23 16:51:28 -0500

    el esamen simple para una nota simple

---
+++ examen.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 	<title></title>
+ 	<link rel="stylesheet" href="public/css/bootstrap.css">
+ 	<link rel="stylesheet" href="public/css/estilo.css">
+ 
+ </head>
+ <body>
+ <div class="container">
+ <nav class="navbar navbar-default" role="navigation">
+
+<h1>MI GALERIA DE MUSICA Y VIDEO.</h1>
+
+ <div class="container" style="margin-top: 90px:" media="screen">
+	<button class="btn btn-info" data-toggle="modal" data-target="#miventana">
+	misica y videos favoritos.
+	</button>
+	<div class="modal fade" id="miventana" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">>
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4>pak de musicas y videos</h4>				
+				</div>
+				<div class="modal-body">
+
+
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+					<video src="public/videos/1.mp4" autobuffer autoloop loop controls poster="/images/video.png"></video>
+
+
+
+					<div class="item">
+ 						<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/IMAGENES/4.jpg">
+ 						<div class="container">
+ 							<div class="carousel-caption">
+ 							
+ 							</div>
+ 						</div>
+ 					</div>
+
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-primary" data-dismiss="modal">CERRAR</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+ <div>
+ <div class="container">
+ 	<div class="row">
+ 		<div class="col-md-6">
+ 			<div class="mi-video">
+ 				<video autobuffer autoloop loop controls>
+ 					<source src="public/videos/1.mp4">
+ 				</video>
+ 			</div>
+ 		</div>
+ 		<div class="col-md-6">
+ 			<div class="mi-video" >
+ 				<video autobuffer autoloop loop controls >
+ 					<source src="public/videos/1.mp4">
+ 				</video>
+ 			</div>
+ 		</div> 
+ 	</div>
+ </div>
+ </div>
+
+ 	<<script src="public/js/jquery-1.12.1.js"></script>
+ 	<<script src="public/js/bootstrap.js"></script>
+
+ <div class="container"> 
+<audio autoplay="false" src="public/audio/EXO-K_Special Stage 'abor a Mi'_KBS MUSIC BANK in MEXICO_2014.11.12.mp3" ></audio>
+ </div>
+
+
+
+
+
+
+
+
+
+
+
+ </body>
+ </html> 
